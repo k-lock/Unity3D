141f0d06805f651f0cabdda1216b154b6c5adaec
k-lock <kerdman@online.de>
2012-12-02 14:19:53 +0100

    refresh content

---
+++ Asset Export V.1/AssetExport.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace klock.snippetz
+{
+    /** Class to creates a compressed file that contains a collection of assets*/
+    public class AssetExport
+    {
+        /** Builds an asset bundle from the selected objects in the project view. Track dependencies.*/
+        [MenuItem("Assets/Build AssetBundle - Track dependencies")]
+        public static void ExportAsset()
+        {
+            string path = EditorUtility.SaveFilePanel("Save object as", "", "Object_", "*.unity3d; *.fbx;");
+            if (path.Length != 0)
+            {
+                Object[] selection = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+                BuildPipeline.BuildAssetBundle(Selection.activeObject, selection, path, BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets);
+                Selection.objects = selection;
+            }
+        }
+        /** Builds an asset bundle from the selected objects in the project view. No dependency tracking. */
+        [MenuItem("Assets/Build AssetBundle - No dependency tracking")]
+        public static void ExportResourceNoTrack()
+        {
+            // Bring up save panel
+            string path = EditorUtility.SaveFilePanel("Save Resource", "", "New Resource", "*.unity3d; *.fbx;");
+            if (path.Length != 0)
+            {
+                // Build the resource file from the active selection.
+                BuildPipeline.BuildAssetBundle(Selection.activeObject, Selection.objects, path);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Contains projects of:
 
  * **ResourceLabV.1 **- A sample visual studio project to create a bitmap containing dll source file. For runtime using in a unity application.
  * **GUI Elements V.1**- A package with scripted gui elements for a unity application.
- 
+ * **Asset Export V.1**- A Unity3D Editor MenuItem class to create a compressed file that contains a collection of assets.
+
  and ...
