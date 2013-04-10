d2ed70ce310c6c6a3b68f28143e80ff35e2331a7
k-lock <kerdman@online.de>
2013-04-10 17:02:13 +0200

    Lock GameObject in SceneView

--- kPoly/FreezeObject.cs
+++ kPoly/FreezeObject.cs
@@ -7,51 +7,51 @@ public class FreezeObject : EditorWindow
 	public 	static	FreezeObject 	instance;
 	private 		int 			_sIndex = -1;
 	private			Transform		_sTrans = null;
+	private			GameObject		_sObjec = null;
 	private			bool			_freeze = false;
 	
-	[MenuItem("Window/klock/kMesh/FreezEditor")]
+	[MenuItem("Window/klock/FreezEditor")]
 	public static void Init ()
 	{
 		instance = (FreezeObject)EditorWindow.GetWindow (typeof(FreezeObject), false, "FreezEditor");
+		
 		instance.Show ();
 		instance.OnEnable ();
-		instance.position = new Rect (200, 100, 200, 250);
-		instance.maxSize = new Vector2 (210, 260);
-		instance.minSize = new Vector2 (190, 240);
+		
+		instance.position = new Rect (200, 100, 200, 30);
+		instance.maxSize = new Vector2 (210, 30);
+		instance.minSize = new Vector2 (190, 30);
 	}
 	
 	void OnEnable ()
 	{
 		if (instance == null)
 			instance = this;
+		
+		OnSelectionChange ();
 	}
 	
-	void OnDisable()
+	void OnDisable ()
 	{
 		instance = null;
 	}
 	
 	void OnSelectionChange ()
 	{
-		if (!_freeze && Selection.activeInstanceID > 0) {
-		
-			_sTrans = Selection.activeTransform;
-			_sIndex = Selection.activeInstanceID;
-		
+		if (!_freeze && Selection.activeInstanceID > -1) {
+			ResetSelection ();
+			GetSelection ();		
 		}
 		
-		if ( _freeze && Selection.activeInstanceID != _sIndex) {
-	
-			Selection.activeTransform = _sTrans;
-			Selection.activeInstanceID = _sIndex;
-			Selection.activeGameObject = _sTrans.gameObject;
-			
+		if (_freeze && Selection.activeInstanceID != _sIndex) {
+			SetSelection ();			
 		}
 	}
-
+	
 	void OnInspectorUpdate ()
 	{
-		if ( _freeze && Selection.activeInstanceID != _sIndex) OnSelectionChange ();
+		if (_freeze && Selection.activeInstanceID != _sIndex)
+			OnSelectionChange ();
 	}
 
 	void OnGUI ()
@@ -63,18 +63,40 @@ public class FreezeObject : EditorWindow
 	{
 		EditorGUILayout.BeginVertical (new GUIStyle { contentOffset = new Vector2 (-10, 0) });
 		EditorGUILayout.Space ();
-		
-		// current selection idendifier
-		EditorGUILayout.ObjectField ("Selection ", _sTrans, typeof(Transform), true);
-		EditorGUILayout.Space ();
-		
+
 		// toggle freeze mode
 		GUI.color = (_freeze) ? Color.yellow : Color.white;
-		if (GUILayout.Button ("Freeze")) {
+		if (GUILayout.Button ("Freeze ")) {
+			ResetSelection ();	
+			
 			_freeze = !_freeze;
+			
+			if (_freeze)
+				GetSelection ();
 		}		
 		GUI.color = Color.white;
 		
 		EditorGUILayout.EndVertical ();
+	}
+
+	void SetSelection ()
+	{
+		Selection.activeTransform = _sTrans;
+		Selection.activeInstanceID = _sIndex;
+		Selection.activeGameObject = _sObjec;
+	}
+
+	void GetSelection ()
+	{
+		_sTrans = Selection.activeTransform;
+		_sIndex = Selection.activeInstanceID;
+		_sObjec = Selection.activeGameObject;
+	}
+	
+	void ResetSelection ()
+	{
+		_sTrans = null;
+		_sIndex = -1;
+		_sObjec = null;
 	}
 }
