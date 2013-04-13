5022cc608f09bb9d0502d45316518cda07b04889
k-lock <kerdman@online.de>
2013-04-13 20:14:57 +0200

    Mesh creation

--- kPoly/CreateCube.cs
+++ kPoly/CreateCube.cs
@@ -5,33 +5,92 @@ using System.Collections.Generic;
 
 class CreateCube
 {
-    public static GameObject CreateMesh(
+	public static GameObject CreateMesh (
                                           string _meshName,
-                                          /*int _uSegments,
-                                          int _vSegments,*/
+                                          int _uSegments,
+                                          int _vSegments,
+                                          int _zSegments,
                                           float _width,
                                           float _height,
                                           float _deep
                                         /*  int _faceIndex,
                                           int _windinIndex,
                                           int _pivotIndex*/)
-    {
+	{
 
-        GameObject quad = new GameObject();
+		GameObject quad = new GameObject ();
 
-        if (!string.IsNullOrEmpty(_meshName))
-            quad.name = _meshName;
-        else
-            quad.name = "kPolyCube";
+		if (!string.IsNullOrEmpty (_meshName))
+			quad.name = _meshName;
+		else
+			quad.name = "kPolyCube";
 
-        quad.transform.position = Vector3.zero;
+		quad.transform.position = Vector3.zero;
+        
+		int xCount = _uSegments + 1;
+		int yCount = _vSegments + 1;
+		int zCount = _zSegments + 1;
+		int vCount = 4 * 6;
+		int tCount = 6;
+		Vector3[] vertices = new Vector3[vCount];
+		Vector2[] uvs = new Vector2[vCount];
+		int[] triangles = new int[tCount];
 
-        int vCount = 4 * 6;
-        int tCount = 6;
-        Vector3[] vertices = new Vector3[vCount];
-        Vector2[] uvs = new Vector2[vCount];
-        int[] triangles = new int[tCount];
+		int index = 0;
+		float xUV = 1.0f / _uSegments;
+		float yUV = 1.0f / _vSegments;
+		float zUV = 1.0f / _zSegments;
+		float xSC = _width / _uSegments;
+		float ySC = _height / _vSegments;
+		float zSC = _height / _zSegments;
 
+	/*	// Vector2 pivot = CreatePlane.PivotVector(_pivotIndex, _width, _height);
+		for (float z = 0.0f; z<zCount; z++) {
+			for (float y = 0.0f; y < yCount; y++) {
+				for (float x = 0.0f; x < xCount; x++) {
+
+					float dx = x * xSC - _width * .5f;// - pivot.x * .5f;
+					float dy = y * ySC - _height * .5f;// - pivot.y * .5f;
+					float dz = z * zSC - _deep * .5f;// - pivot.y * .5f;
+					Vector3 vert = new Vector3( dx, dy, dz);
+					
+					vertices [index] = vert;
+					uvs[index++] = new Vector2 (x * xUV, y * yUV);
+				}
+			}
+		}*/
+
+		for (int _faceIndex = 0; _faceIndex < 6; _faceIndex++) {
+			for (float y = 0.0f; y < yCount; y++) {
+				for (float x = 0.0f; x < xCount; x++) {
+
+					float dx = x * xSC - _width * .5f;// - pivot.x * .5f;
+					float dy = y * ySC - _height * .5f;// - pivot.y * .5f;
+					float dz = y * zSC - _deep * .5f;// - pivot.y * .5f;
+					
+					Vector3 vert = Vector3.zero;
+					if (_faceIndex == 0) {
+						vert = new Vector3 (dx, _deep*.5f, dy); // xz
+					} else if (_faceIndex == 1) {
+						vert = new Vector3 (dx, dy, _deep*.5f); //xy
+					} else if (_faceIndex == 2) {
+						vert = new Vector3 (_deep*.5f, dy, dx); //zy
+					} else if (_faceIndex == 3) {
+						vert = new Vector3 (dy, -_deep*.5f, dx); //zx
+					} else if (_faceIndex == 4) {
+						vert = new Vector3 (dy, dx, -_deep*.5f); //yx
+					} else if (_faceIndex == 5) {
+						vert = new Vector3 (-_deep*.5f, dx, dy); //yz
+					}
+
+					vertices [index] = vert;
+				//	Debug.Log(vert);
+					uvs [index++] = new Vector2 (x * xUV, y * yUV);
+				}
+			}
+		}
+		/*	// xy,zy,xy,zy,zx,zx
+		
         vertices = new Vector3[]{
 			// face 1 (xy plane, z=0)
 			new Vector3(0,0,0), 
@@ -63,45 +122,86 @@ class CreateCube
 			new Vector3(0,0,_deep), 
 			new Vector3(_width,0,_deep), 
 			new Vector3(_width,0,0), 
-		};
-        int faces = 6; // here a face = 2 triangles
+		};*/
+		int faces = 6; // here a face = 2 triangles
         
-        List<int> trianglesL = new List<int>();
-        List<Vector2> uvsL = new List<Vector2>();
+		List<int> trianglesL = new List<int> ();
+		List<Vector2> uvsL = new List<Vector2> ();
 
-        for (int i = 0; i < faces; i++)
+		for (int i = 0; i < faces; i++) {
+			int triangleOffset = i * 4;
+			trianglesL.Add (0 + triangleOffset);
+			trianglesL.Add (2 + triangleOffset);
+			trianglesL.Add (1 + triangleOffset);
+
+			trianglesL.Add (0 + triangleOffset);
+			trianglesL.Add (3 + triangleOffset);
+			trianglesL.Add (2 + triangleOffset);
+
+			// same uvs for all faces
+			uvsL.Add (new Vector2 (0, 0));
+			uvsL.Add (new Vector2 (1, 0));
+			uvsL.Add (new Vector2 (1, 1));
+			uvsL.Add (new Vector2 (0, 1));
+		}
+
+		MeshFilter mf = quad.AddComponent<MeshFilter> ();
+		quad.AddComponent<MeshRenderer> ();  
+		Mesh m = new Mesh ();
+
+		m.name = quad.name;
+		m.vertices = vertices;
+		m.uv = uvsL.ToArray ();
+		m.triangles = trianglesL.ToArray ();
+
+		mf.sharedMesh = m;
+
+		m.RecalculateNormals ();
+		m.RecalculateBounds ();
+		m.Optimize ();
+		
+		CreatePlane.AddCollider (1, quad, m);
+
+		return quad;
+	}
+	/* public static Vector3 PivotVector
+        (int _pivotIndex,
+        float _width,
+        float _height,
+        float _deep )
+    {
+        Vector3 p = Vector3.zero;
+        switch (_pivotIndex)
         {
-            int triangleOffset = i * 4;
-            trianglesL.Add(0 + triangleOffset);
-            trianglesL.Add(2 + triangleOffset);
-            trianglesL.Add(1 + triangleOffset);
-
-            trianglesL.Add(0 + triangleOffset);
-            trianglesL.Add(3 + triangleOffset);
-            trianglesL.Add(2 + triangleOffset);
-
-            // same uvs for all faces
-            uvsL.Add(new Vector2(0, 0));
-            uvsL.Add(new Vector2(1, 0));
-            uvsL.Add(new Vector2(1, 1));
-            uvsL.Add(new Vector2(0, 1));
+            case 0://TOP FRONT LEFT:
+                p = new Vector3(-_width, _height, 0);
+                break;
+            case 1://TOP FRONT Center:
+                p = new Vector3(0, _height, 0);
+                break;
+            case 2://TOP FRONT Right:
+                p = new Vector3(_width, _height, 0);
+                break;
+            case 3://PIVOT FRONT Left:
+                p = new Vector3(-_width, 0, 0);
+                break;
+            case 4://PIVOT FRONT Center:
+                p = new Vector3(0, 0, _deep * .5f);
+                break;
+            case 5://TextAnchor.MiddleRight:
+                p = new Vector2(_width, 0);
+                break;
+            case 6://TextAnchor.LowerLeft:
+                p = new Vector2(-_width, -_height);
+                break;
+            case 7://TextAnchor.LowerCenter:
+                p = new Vector2(0, -_height);
+                break;
+            case 8://TextAnchor.LowerRight:
+                p = new Vector2(_width, -_height);
+                break;
         }
+        return p;
 
-        MeshFilter mf = quad.AddComponent<MeshFilter>();
-        quad.AddComponent<MeshRenderer>();  
-        Mesh m = new Mesh();
-
-        m.name = quad.name;
-        m.vertices = vertices;
-        m.uv = uvsL.ToArray();
-        m.triangles = trianglesL.ToArray();
-
-        mf.sharedMesh = m;
-
-        m.RecalculateNormals();
-        m.RecalculateBounds();
-        m.Optimize();
-
-        return quad;
-    }
+    }*/
 }
--- kPoly/CreatePlane.cs
+++ kPoly/CreatePlane.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public class CreatePlane
 {
-    /** Create plane mesh in a unity gameobject.
+	/** Create plane mesh in a unity gameobject.
      * 
      *  @params string - A string value for the gameobject.name
      *  @params int - The count of horizontal segments.
@@ -16,166 +16,182 @@ public class CreatePlane
      *  
      *  @return GameObject - The generated object.
      */
-    public static GameObject CreateMesh(string _meshName,
+	public static GameObject CreateMesh (string _meshName,
                                   int _uSegments,
                                   int _vSegments,
                                   float _width,
                                   float _height,
                                   int _faceIndex,
                                   int _windinIndex,
-                                  int _pivotIndex)
-    {
-        //string assetName = "Assets/quad.asset";
+                                  int _pivotIndex,
+								  int _colliderIndex)
+	{
+		//string assetName = "Assets/quad.asset";
 
-        GameObject quad = new GameObject();
+		GameObject quad = new GameObject ();
 
-        if (!string.IsNullOrEmpty(_meshName))
-            quad.name = _meshName;
-        else
-            quad.name = "kPoly";
+		if (!string.IsNullOrEmpty (_meshName))
+			quad.name = _meshName;
+		else
+			quad.name = "kPoly";
 
-        quad.transform.position = Vector3.zero;
+		quad.transform.position = Vector3.zero;
 
-        int xCount = _uSegments + 1;
-        int yCount = _vSegments + 1;
-        int tCount = _uSegments * _vSegments * 6;
-        int vCount = xCount * yCount;
+		int xCount = _uSegments + 1;
+		int yCount = _vSegments + 1;
+		int tCount = _uSegments * _vSegments * 6;
+		int vCount = xCount * yCount;
 
-        Vector3[] vertices = new Vector3[vCount];
-        Vector2[] uvs = new Vector2[vCount];
-        int[] triangles = new int[tCount];
+		Vector3[] vertices = new Vector3[vCount];
+		Vector2[] uvs = new Vector2[vCount];
+		int[] triangles = new int[tCount];
 
-        int index = 0;
-        float xUV = 1.0f / _uSegments;
-        float yUV = 1.0f / _vSegments;
-        float xSC = _width / _uSegments;
-        float ySC = _height / _vSegments;
+		int index = 0;
+		float xUV = 1.0f / _uSegments;
+		float yUV = 1.0f / _vSegments;
+		float xSC = _width / _uSegments;
+		float ySC = _height / _vSegments;
 
-        Vector2 pivot = PivotVector(_pivotIndex, _width, _height);
-        //Vector2 windin = TriangleWinding;
+		Vector2 pivot = PivotVector (_pivotIndex, _width, _height);
+		//Vector2 windin = TriangleWinding;
 
-        for (float y = 0.0f; y < yCount; y++)
-        {
-            for (float x = 0.0f; x < xCount; x++)
-            {
+		for (float y = 0.0f; y < yCount; y++) {
+			for (float x = 0.0f; x < xCount; x++) {
 
-                float dx = x * xSC - _width * .5f - pivot.x * .5f;
-                float dy = y * ySC - _height * .5f - pivot.y * .5f;
+				float dx = x * xSC - _width * .5f - pivot.x * .5f;
+				float dy = y * ySC - _height * .5f - pivot.y * .5f;
 
-                vertices[index] = (_faceIndex == 0) ? new Vector3(dx, 0.0f, dy) : new Vector3(dx, dy, 0.0f);
-                uvs[index++] = new Vector2(x * xUV, y * yUV);
-            }
-        }
+				vertices [index] = (_faceIndex == 0) ? new Vector3 (dx, 0.0f, dy) : new Vector3 (dx, dy, 0.0f);
+				uvs [index++] = new Vector2 (x * xUV, y * yUV);
+			}
+		}
 
-        index = 0;
-        for (int y = 0; y < _vSegments; y++)
-        {
-            for (int x = 0; x < _uSegments; x++)
-            {
+		index = 0;
+		for (int y = 0; y < _vSegments; y++) {
+			for (int x = 0; x < _uSegments; x++) {
 
-                int p1 = (y * xCount) + x;
-                int p2 = (y * xCount) + x + 1;
-                int p3 = ((y + 1) * xCount) + x;
-                int p4 = ((y + 1) * xCount) + x + 1;
+				int p1 = (y * xCount) + x;
+				int p2 = (y * xCount) + x + 1;
+				int p3 = ((y + 1) * xCount) + x;
+				int p4 = ((y + 1) * xCount) + x + 1;
 
-                switch (_windinIndex)
-                {
-                    case 0:
-                        triangles[index] = p3;
-                        triangles[index + 1] = p2;
-                        triangles[index + 2] = p1;
+				switch (_windinIndex) {
+				case 0:
+					triangles [index] = p3;
+					triangles [index + 1] = p2;
+					triangles [index + 2] = p1;
 
-                        triangles[index + 3] = p3;
-                        triangles[index + 4] = p4;
-                        triangles[index + 5] = p2;
-                        break;
-                    case 1:
-                        triangles[index] = p4;
-                        triangles[index + 1] = p1;
-                        triangles[index + 2] = p3;
+					triangles [index + 3] = p3;
+					triangles [index + 4] = p4;
+					triangles [index + 5] = p2;
+					break;
+				case 1:
+					triangles [index] = p4;
+					triangles [index + 1] = p1;
+					triangles [index + 2] = p3;
 
-                        triangles[index + 3] = p4;
-                        triangles[index + 4] = p2;
-                        triangles[index + 5] = p1;
-                        break;
-                    case 2:
-                        triangles[index] = p1;
-                        triangles[index + 1] = p4;
-                        triangles[index + 2] = p2;
+					triangles [index + 3] = p4;
+					triangles [index + 4] = p2;
+					triangles [index + 5] = p1;
+					break;
+				case 2:
+					triangles [index] = p1;
+					triangles [index + 1] = p4;
+					triangles [index + 2] = p2;
 
-                        triangles[index + 3] = p1;
-                        triangles[index + 4] = p3;
-                        triangles[index + 5] = p4;
-                        break;
-                    case 3:
-                        triangles[index] = p2;
-                        triangles[index + 1] = p3;
-                        triangles[index + 2] = p4;
+					triangles [index + 3] = p1;
+					triangles [index + 4] = p3;
+					triangles [index + 5] = p4;
+					break;
+				case 3:
+					triangles [index] = p2;
+					triangles [index + 1] = p3;
+					triangles [index + 2] = p4;
 
-                        triangles[index + 3] = p2;
-                        triangles[index + 4] = p1;
-                        triangles[index + 5] = p3;
-                        break;
-                }
-                index += 6;
-            }
-        }
+					triangles [index + 3] = p2;
+					triangles [index + 4] = p1;
+					triangles [index + 5] = p3;
+					break;
+				}
+				index += 6;
+			}
+		}
 
-        MeshFilter mf = quad.AddComponent<MeshFilter>();
-        quad.AddComponent<MeshRenderer>(); //MeshRenderer mr = 
-        Mesh m = new Mesh();
+		MeshFilter mf = quad.AddComponent<MeshFilter> ();
+		quad.AddComponent<MeshRenderer> (); //MeshRenderer mr = 
+		Mesh m = new Mesh ();
 
-        m.name = quad.name;
-        m.vertices = vertices;
-        m.uv = uvs;
-        m.triangles = triangles;
+		m.name = quad.name;
+		m.vertices = vertices;
+		m.uv = uvs;
+		m.triangles = triangles;
 
-        mf.sharedMesh = m;
+		mf.sharedMesh = m;
 
-        m.RecalculateNormals();
-        m.RecalculateBounds();
-
-        return quad;
-    }
-    /** Returns a vector2 containing the new position for the transform point of the generated mesh.*/
-    private static Vector2 PivotVector
+		m.RecalculateNormals ();
+		m.RecalculateBounds ();
+		
+		AddCollider (_colliderIndex, quad, m);
+		
+		return quad;
+	}
+	/** Returns a vector2 containing the new position for the transform point of the generated mesh.*/
+	private static Vector2 PivotVector
             (int _pivotIndex,
             float _width,
             float _height)
-    {
-        Vector2 p = Vector2.zero;
-        switch (_pivotIndex)
-        {
-            case 0://TextAnchor.UpperLeft:
-                p = new Vector2(-_width, _height);
-                break;
-            case 1://TextAnchor.UpperCenter:
-                p = new Vector2(0, _height);
-                break;
-            case 2://TextAnchor.UpperRight:
-                p = new Vector2(_width, _height);
-                break;
-            case 3://TextAnchor.MiddleLeft:
-                p = new Vector2(-_width, 0);
-                break;
-            case 4://TextAnchor.MiddleCenter:
-                p = Vector2.zero;
-                break;
-            case 5://TextAnchor.MiddleRight:
-                p = new Vector2(_width, 0);
-                break;
-            case 6://TextAnchor.LowerLeft:
-                p = new Vector2(-_width, -_height);
-                break;
-            case 7://TextAnchor.LowerCenter:
-                p = new Vector2(0, -_height);
-                break;
-            case 8://TextAnchor.LowerRight:
-                p = new Vector2(_width, -_height);
-                break;
-        }
-        return p;
+	{
+		Vector2 p = Vector2.zero;
+		switch (_pivotIndex) {
+		case 0://TextAnchor.UpperLeft:
+			p = new Vector2 (-_width, _height);
+			break;
+		case 1://TextAnchor.UpperCenter:
+			p = new Vector2 (0, _height);
+			break;
+		case 2://TextAnchor.UpperRight:
+			p = new Vector2 (_width, _height);
+			break;
+		case 3://TextAnchor.MiddleLeft:
+			p = new Vector2 (-_width, 0);
+			break;
+		case 4://TextAnchor.MiddleCenter:
+			p = Vector2.zero;
+			break;
+		case 5://TextAnchor.MiddleRight:
+			p = new Vector2 (_width, 0);
+			break;
+		case 6://TextAnchor.LowerLeft:
+			p = new Vector2 (-_width, -_height);
+			break;
+		case 7://TextAnchor.LowerCenter:
+			p = new Vector2 (0, -_height);
+			break;
+		case 8://TextAnchor.LowerRight:
+			p = new Vector2 (_width, -_height);
+			break;
+		}
+		return p;
 
-    }
+	}
+	/** Add a collider object to the generated gameobject.
+	 *  @params GameObject quad - The target gameobject.
+	 * 	@params Mesh m - The sharedMesh property for a MeshCollider componete.
+	 */
+	public static void AddCollider (int colIndex, GameObject quad, Mesh m)
+	{
+		if (colIndex > 0) {
+				
+			switch (colIndex) {
+			case 1: // mesh 
+				MeshCollider mc = quad.AddComponent<MeshCollider> ();
+				mc.sharedMesh = m;
+				break;
+			case 2: // box
+				quad.AddComponent<BoxCollider> ();
+				break;
+			}				
+		}
+	}
 }
 
