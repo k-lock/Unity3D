d0d418a8e27069ea476f8077f8813b897be14a1a
k-lock <kerdman@online.de>
2013-04-20 18:24:43 +0200

    kPoly Editor - Styles - V.1.3.1

---
+++ kPoly/style/KP_Style.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace klock.kEditPoly.style
+{
+    public class KP_Style
+    {
+        public static GUIStyle tf_input_center()
+        {
+            GUIStyle cs = new GUIStyle(EditorStyles.textField);
+            cs.alignment = TextAnchor.MiddleCenter;
+
+            return cs;
+        }
+        public static GUIStyle grid()
+        {
+            GUIStyle cs = new GUIStyle(GUI.skin.button);
+            // cs.fontSize = 9;
+            cs.contentOffset = Vector2.zero;
+            return cs;
+        }
+
+    }
+}
