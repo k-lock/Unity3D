ac8d74fb5cc75de10f621795c18d28df9cc773f1
k-lock <kerdman@online.de>
2013-04-24 00:41:43 +0200

    kPoly Editor - Base Window - V.1.3.1

--- kPoly/kPoly2Tool.cs
+++ kPoly/kPoly2Tool.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using klock.kEditPoly.panels;
 using klock.kEditPoly.helper;
 
@@ -9,7 +12,7 @@ namespace klock.kEditPoly
     {
         #region vars
         public static kPoly2Tool instance;
-        private SceneView.OnSceneFunc _onSceneGUI_ = null;
+        private static SceneView.OnSceneFunc _onSceneGUI_ = null;
 
         public int MAIN_MENU_ID = 0;
 
@@ -38,11 +41,18 @@ namespace klock.kEditPoly
             {
                 instance = this;
             }
-
+            _onSceneGUI_ = new SceneView.OnSceneFunc(OnSceneGUI);
         }
 
         private void Update()
         {
+            if (KP_edit._editorMode != MODE.None)
+            {
+                if (KP_edit._freeze && Selection.activeGameObject != KP_edit._selection)
+                {
+                    Selection.activeGameObject = KP_edit._selection;
+                }
+            }
         }
 
         private void OnGUI()
@@ -57,11 +67,16 @@ namespace klock.kEditPoly
 
         public void OnSceneGUI(SceneView sceneView)
         {
-
+            if (KP_edit._editorMode != MODE.None && KP_edit._freeze)
+            {
+                KP_edit.Draw_Handles();
+            }
         }
         private void OnDisable()
         {
             instance = null;
+            _onSceneGUI_ = null;
+            KP_material.ME_LIST = null;
         }
         private void OnDestroy()
         {
@@ -72,32 +87,43 @@ namespace klock.kEditPoly
 
         private void DrawWindow()
         {
-            int controlID = GUIUtility.GetControlID(instance.GetHashCode(), FocusType.Passive);
-            Event e = Event.current;
-            switch (e.GetTypeForControl(controlID))
+            /* int controlID = GUIUtility.GetControlID(instance.GetHashCode(), FocusType.Passive);
+             Event e = Event.current;
+             switch (e.GetTypeForControl(controlID))
+             {
+                 case EventType.mouseDown:
+                 case EventType.mouseUp:
+                 case EventType.mouseDrag:
+                 case EventType.keyUp:
+                 case EventType.keyDown:
+                 case EventType.repaint:
+                 case EventType.layout:
+                 case EventType.ExecuteCommand:
+                 case EventType.ValidateCommand:*/
+
+            //kPolyGUI.MAIN_TOOLBAR();
+            KP_mainTool.DRAW_BAR();
+
+            switch (MAIN_MENU_ID)
             {
-                case EventType.mouseDown:
-                case EventType.mouseUp:
-                case EventType.mouseDrag:
-                case EventType.keyUp:
-                case EventType.keyDown:
-                case EventType.repaint:
-                case EventType.layout:
-                case EventType.ExecuteCommand:
-                case EventType.ValidateCommand:
+                case 0: KP_create.DRAW_PANEL(); break;
+                case 1: KP_edit.DRAW_PANEL(); KP_edit.CHECK_USER_INPUT(); break;
+                case 2: KP_info.DRAW_PANEL(); break;
+                case 3: KP_material.DRAW_PANEL(); break;
+            }
 
-                    //kPolyGUI.MAIN_TOOLBAR();
-                    KP_mainTool.DRAW_BAR();
-
-                    switch (MAIN_MENU_ID)
-                    {
-                        case 0: KP_create.DRAW_PANEL(); break;
-                        case 1: break;
-                        case 2: KP_info.DRAW_PANEL(); break;
-                        case 3: KP_material.DRAW_PANEL(); break;
-                    }
-
-                    break;
+            /*       break;
+           }*/
+        }
+        public static void SceneEvent(bool state)
+        {
+            if (state)
+            {
+                SceneView.onSceneGUIDelegate += _onSceneGUI_;
+            }
+            else
+            {
+                SceneView.onSceneGUIDelegate -= _onSceneGUI_;
             }
         }
     }//class
