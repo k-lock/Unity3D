70e6bab268bfc158cae0a89eac68314aeb32f1c3
k-lock <kerdman@online.de>
2013-04-24 15:44:30 +0200

    kPoly Editor - Panels - V.1.3.1

--- kPoly/panels/KP_edit.cs
+++ kPoly/panels/KP_edit.cs
@@ -28,6 +28,7 @@ namespace klock.kEditPoly.panels
         static PLANAR_HELPER planarHelp = new PLANAR_HELPER();
         public static MODE _editorMode = MODE.None;
 
+        static int triIndex = -1;
         static List<int> curPointIndex = new List<int>();
         static Vector3[] verts;
 
@@ -38,68 +39,80 @@ namespace klock.kEditPoly.panels
             EditorGUILayout.BeginVertical();
             GUILayout.Space(5);
 
-            _selection = kSelect.OBJECT;
+            if (_selection == null) _selection = kSelect.OBJECT;
             Mesh _selectMesh = kSelect.MESH;
+            MODE modeTemp = _editorMode;
             GUI.enabled = (_selection != null && _selectMesh != null);
             //-------------------------------------------------------------------
             //  SELECTION
             FOLD_selection = EditorGUILayout.Foldout(FOLD_selection, "Selection ");// + ((_selection != null) ? "[ " + _selection.name + " ]" : ""));
             if (FOLD_selection)
             {
+                bool pressed = false;
+
                 GUILayout.BeginHorizontal();
-                GUI.color = (_editorMode == MODE.E_Point) ? new Color(0, .5f, 1, .7f) : Color.white;
+                GUI.color = (_editorMode == MODE.Point) ? new Color(0, .5f, 1, .7f) : Color.white;
                 if (GUILayout.Button(new GUIContent("Point")))
                 {
-                    _editorMode = (_editorMode == MODE.E_Point) ? MODE.None : MODE.E_Point;
-
-                    if (_selection != null)
-                    {
-                        _freeze = !_freeze;
-
-                        if (_editorMode == MODE.E_Point)
-                        {
-                            _selection.hideFlags |= HideFlags.NotEditable;
-                            kPoly2Tool.SceneEvent(true);
-                        }
-                        else
-                        {
-                            _selection.hideFlags = 0;
-                            kPoly2Tool.SceneEvent(false);
-                        }
-                    }
+                    _editorMode = (_editorMode == MODE.Point) ? MODE.None : MODE.Point;
+                    pressed = true;
                 }
                 GUI.color = Color.white;
-                GUI.color = (_editorMode == MODE.E_Line) ? new Color(0, .5f, 1, .7f) : Color.white;
+                GUI.color = (_editorMode == MODE.Line) ? new Color(0, .5f, 1, .7f) : Color.white;
                 if (GUILayout.Button(new GUIContent("Line")))
                 {
-                    _editorMode = (_editorMode == MODE.E_Line) ? MODE.None : MODE.E_Line;
-
-                    if (_selection != null)
-                    {
-                        _freeze = !_freeze;
-
-                        if (_editorMode == MODE.E_Line)
-                        {
-                            _selection.hideFlags |= HideFlags.NotEditable;
-                            kPoly2Tool.SceneEvent(true);
-                        }
-                        else
-                        {
-                            _selection.hideFlags = 0;
-                            kPoly2Tool.SceneEvent(false);
-                        }
-                    }
+                    _editorMode = (_editorMode == MODE.Line) ? MODE.None : MODE.Line;
+                    pressed = true;
                 }
                 GUI.color = Color.white;
-                GUI.color = (_editorMode == MODE.E_Quad) ? new Color(0, .5f, 1, .7f) : Color.white;
-                if (GUILayout.Button(new GUIContent("Segment")))
-                    _editorMode = (_editorMode == MODE.E_Quad) ? MODE.None : MODE.E_Quad;
+                GUI.color = (_editorMode == MODE.Tri) ? new Color(0, .5f, 1, .7f) : Color.white;
+                if (GUILayout.Button(new GUIContent("Tri")))
+                {
+                    _editorMode = (_editorMode == MODE.Tri) ? MODE.None : MODE.Tri;
+                    pressed = true;
+                }
                 GUI.color = Color.white;
-                GUI.color = (_editorMode == MODE.E_All) ? new Color(0, .5f, 1, .7f) : Color.white;
+                GUI.color = (_editorMode == MODE.Quad) ? new Color(0, .5f, 1, .7f) : Color.white;
+                if (GUILayout.Button(new GUIContent("Quad")))
+                {
+                    _editorMode = (_editorMode == MODE.Quad) ? MODE.None : MODE.Quad;
+                    pressed = true;
+                }
+                GUI.color = Color.white;
+                GUI.color = (_editorMode == MODE.All) ? new Color(0, .5f, 1, .7f) : Color.white;
                 if (GUILayout.Button(new GUIContent("Complete")))
-                    _editorMode = (_editorMode == MODE.E_All) ? MODE.None : MODE.E_All;
+                {
+                    _editorMode = (_editorMode == MODE.All) ? MODE.None : MODE.All;
+                    pressed = true;
+                }
                 GUI.color = Color.white;
                 GUILayout.EndHorizontal();
+
+                if (pressed)
+                {
+                    // if (_selection != null )
+                    // {
+                    _selection = kSelect.OBJECT;
+                    _freeze = (_editorMode != MODE.None) ? true : false;
+
+                    if (_editorMode != MODE.None)
+                    {
+                        _selection.hideFlags |= HideFlags.NotEditable;
+                        kPoly2Tool.SceneEvent(true);
+                    }
+                    else
+                    {
+                        _selection.hideFlags = 0;
+                        kPoly2Tool.SceneEvent(false);
+                    }
+                    curPointIndex.Clear();
+                    //  }
+                }
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PrefixLabel("Vertex ");
+                EditorGUILayout.LabelField((_editorMode != MODE.None && curPointIndex.Count > 0) ? _editorMode.ToString() + " " + curPointIndex[0] : "", GUILayout.ExpandWidth(true));
+                EditorGUILayout.EndHorizontal();
             }
 
             //-------------------------------------------------------------------
@@ -161,27 +174,37 @@ namespace klock.kEditPoly.panels
 
             verts = _selectMesh.vertices;
             Transform root = _selection.transform;
-            bool setDirty = false;
+            float cubeSize = .3f;
+            int controlIDBeforeHandle = -1,
+                controlIDAfterHandle = -1;
+            bool isEventUsedByHandle = false,
+                isEventUsedBeforeHandle = false,
+                setDirty = false;
 
             // Point Mode 
-
+            for (int i = 0; i < verts.Length; i++)
+            {
+                Vector3 v1 = root.TransformPoint(verts[i]);
+                if (KP_info._SHOW_TRIAS) Handles.Label(v1, new GUIContent("" + i));
+            }
+            int someHashCode = kPoly2Tool.instance.GetHashCode();
             switch (_editorMode)
             {
 
-                case MODE.E_Point:
-                    int someHashCode = kPoly2Tool.instance.GetHashCode();
+                case MODE.Point:
+
                     for (int i = 0; i < verts.Length; i++)
                     {
                         Vector3 v1 = root.TransformPoint(verts[i]);
-                        float cubeSize = HandleUtility.GetHandleSize(v1) * .3f;
-                        int controlIDBeforeHandle = GUIUtility.GetControlID(someHashCode, FocusType.Passive);
-                        bool isEventUsedBeforeHandle = (Event.current.type == EventType.used);
+
+                        controlIDBeforeHandle = GUIUtility.GetControlID(someHashCode, FocusType.Passive);
+                        isEventUsedBeforeHandle = (Event.current.type == EventType.used);
 
                         if (curPointIndex.Contains(i))
                             Handles.color = new Color(Color.red.r, Color.red.g, Color.red.b, .85f);
                         else
                             Handles.color = new Color(Color.green.r, Color.green.g, Color.green.b, .85f);
-
+                        cubeSize = HandleUtility.GetHandleSize(v1) * .3f;
                         Handles.ScaleValueHandle(0, v1, Quaternion.identity, cubeSize, Handles.CubeCap, 0);
                         if (curPointIndex.Contains(i))
                         {
@@ -189,8 +212,8 @@ namespace klock.kEditPoly.panels
                             if (v1 != verts[i] && !setDirty) setDirty = true;
                         }
 
-                        int controlIDAfterHandle = GUIUtility.GetControlID(someHashCode, FocusType.Native);
-                        bool isEventUsedByHandle = !isEventUsedBeforeHandle && (Event.current.type == EventType.used);
+                        controlIDAfterHandle = GUIUtility.GetControlID(someHashCode, FocusType.Native);
+                        isEventUsedByHandle = !isEventUsedBeforeHandle && (Event.current.type == EventType.used);
 
                         if ((controlIDBeforeHandle < GUIUtility.hotControl && GUIUtility.hotControl < controlIDAfterHandle) || isEventUsedByHandle)
                         {
@@ -198,8 +221,80 @@ namespace klock.kEditPoly.panels
                         }
                     }
                     break;
-                case MODE.E_Line:
-                   
+                case MODE.Line:
+
+
+                    break;
+                case MODE.Tri:
+
+                    for (int i = 0; i < verts.Length; i++)
+                    {
+                        Vector3 v1 = root.TransformPoint(verts[_selectMesh.triangles[i * 3]]);
+                        Vector3 v2 = root.TransformPoint(verts[_selectMesh.triangles[i * 3 + 1]]);
+                        Vector3 v3 = root.TransformPoint(verts[_selectMesh.triangles[i * 3 + 2]]);
+                        Vector3 dv = (v1 + v2 + v3) / 3;
+
+                        controlIDBeforeHandle = GUIUtility.GetControlID(someHashCode, FocusType.Passive);
+                        isEventUsedBeforeHandle = (Event.current.type == EventType.used);
+
+                        if (curPointIndex.Contains(i))
+                            Handles.color = new Color(Color.red.r, Color.red.g, Color.red.b, .85f);
+                        else
+                            Handles.color = new Color(Color.green.r, Color.green.g, Color.green.b, .85f);
+
+                        cubeSize = HandleUtility.GetHandleSize(v1) * .3f;
+
+                        Handles.DrawPolyLine(new Vector3[4] { v1, v2, v3, v1 });
+
+
+                        Handles.ScaleValueHandle(0, dv, Quaternion.identity, cubeSize, Handles.DotCap, 0);
+                        if (curPointIndex.Contains(i))
+                        {
+
+                            Vector3 dm = Handles.PositionHandle(dv, Quaternion.identity);
+                            if (dm != dv && !isEventUsedBeforeHandle)
+                            {
+
+                                Vector3 d = dm - dv;
+                                verts[_selectMesh.triangles[i * 3]] += d;
+                                verts[_selectMesh.triangles[i * 3 + 1]] += d;
+                                verts[_selectMesh.triangles[i * 3 + 2]] += d;
+
+                                setDirty = true;
+                            }
+                        }
+
+                        controlIDAfterHandle = GUIUtility.GetControlID(someHashCode, FocusType.Native);
+                        isEventUsedByHandle = !isEventUsedBeforeHandle && (Event.current.type == EventType.used);
+
+                        if ((controlIDBeforeHandle < GUIUtility.hotControl && GUIUtility.hotControl < controlIDAfterHandle) || isEventUsedByHandle)
+                        {
+                            POINT_SELECTION(i);
+                        }
+                    }
+                    break;
+                case MODE.All:
+
+                    Vector3 dav = Vector3.zero;
+                    for (int i = 0; i < verts.Length; i++)
+                    {
+                        Vector3 v1 = root.TransformPoint(verts[i]);
+                        dav += v1;
+                    }
+                    dav = dav / verts.Length;
+                    cubeSize = HandleUtility.GetHandleSize(dav) * .8f;
+                    Handles.ScaleValueHandle(0, dav, Quaternion.identity, cubeSize, Handles.CubeCap, 0);
+                    Vector3 mdav = root.InverseTransformPoint(Handles.PositionHandle(dav, Quaternion.identity));
+                    if (mdav != dav && !setDirty)
+                    {
+                        Vector3 d = mdav - dav;
+                        for (int i = 0; i < verts.Length; i++)
+                        {
+                            verts[i] += d;
+                        }
+                        setDirty = true;
+                    }
+
 
                     break;
             }
@@ -210,13 +305,17 @@ namespace klock.kEditPoly.panels
                 _selectMesh.vertices = verts;
                 _selectMesh.RecalculateNormals();
                 _selectMesh.RecalculateBounds();
+                _selection.GetComponent<MeshCollider>().sharedMesh = _selectMesh;
             }
         }
         private static void POINT_SELECTION(int i)
         {
             if (!ANY_KEY && curPointIndex.Count > 0) curPointIndex.Clear();
             if (!curPointIndex.Contains(i))
+            {
                 curPointIndex.Add(i);
+                kPoly2Tool.instance.Repaint();
+            }
             else
                 curPointIndex.Remove(i);
         }
