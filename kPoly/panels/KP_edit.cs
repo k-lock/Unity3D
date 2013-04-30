52395434b46f1bea6fd5474cf9de65b69fdc7a1a
k-lock <kerdman@online.de>
2013-04-30 21:59:45 +0200

    kPoly Editor - Panels - V1.3.1

--- kPoly/panels/KP_edit.cs
+++ kPoly/panels/KP_edit.cs
@@ -121,7 +121,7 @@ namespace klock.kEditPoly.panels
             if (FOLD_verts)
             {
                 GUILayout.BeginHorizontal();
-                GUILayout.Button(new GUIContent("Remove"));
+                if (GUILayout.Button(new GUIContent("Remove"))) VERT_remove();
                 GUILayout.Button(new GUIContent("Break"));
                 GUILayout.EndHorizontal();
 
@@ -350,28 +350,28 @@ namespace klock.kEditPoly.panels
                     POINT_SELECTION(i);
                 }
 
-             /*   if (curPointIndex.Contains(i))
-                {
-                    Vector3 dv = Vector3.zero;
-                    Vector3 mv = Vector3.zero;
+                /*   if (curPointIndex.Contains(i))
+                   {
+                       Vector3 dv = Vector3.zero;
+                       Vector3 mv = Vector3.zero;
 
-                    foreach (int id in curPointIndex) dv += root.TransformPoint(verts[id]);
+                       foreach (int id in curPointIndex) dv += root.TransformPoint(verts[id]);
 
-                    if ((dv = dv / curPointIndex.Count) != Vector3.zero)
-                    {
-                        mv = Handles.PositionHandle(dv, Quaternion.identity);
-                        if (mv != dv && !setDirty)
-                        {
-                            Vector3 d = mv - dv;
-                            foreach (int id in curPointIndex) verts[id] += d;
-                            setDirty = true;
-                        }
-                    }
-                }*/
+                       if ((dv = dv / curPointIndex.Count) != Vector3.zero)
+                       {
+                           mv = Handles.PositionHandle(dv, Quaternion.identity);
+                           if (mv != dv && !setDirty)
+                           {
+                               Vector3 d = mv - dv;
+                               foreach (int id in curPointIndex) verts[id] += d;
+                               setDirty = true;
+                           }
+                       }
+                   }*/
                 if (curPointIndex.Contains(i) && !isDrawn)
                 {
                     foreach (int id in curPointIndex) dhp += root.TransformPoint(verts[id]);
-                    if ((mpos= dhp = dhp / curPointIndex.Count) != Vector3.zero)
+                    if ((mpos = dhp = dhp / curPointIndex.Count) != Vector3.zero)
                     {
                         switch (Tools.current)
                         {
@@ -386,7 +386,7 @@ namespace klock.kEditPoly.panels
                                 break;
                         }
                         isDrawn = true;
-                        if (mpos != dhp || mrot != Quaternion.identity || scale != new Vector3(1,1,1))//&& !setDirty)
+                        if (mpos != dhp || mrot != Quaternion.identity || scale != new Vector3(1, 1, 1))//&& !setDirty)
                         {
                             Vector3 d = mpos - dhp;
                             if (mrot != Quaternion.identity || scale != new Vector3(1, 1, 1)) d = Vector3.zero;
@@ -442,8 +442,8 @@ namespace klock.kEditPoly.panels
         }
         private static void POINT_SELECTION(int i)
         {
-            
-            
+
+
             if (!ANY_KEY && curPointIndex.Count > 0) curPointIndex.Clear();
             if (!curPointIndex.Contains(i))
             {
@@ -453,7 +453,347 @@ namespace klock.kEditPoly.panels
             else
                 curPointIndex.Remove(i);
         }
-    }
+        private static void VERT_remove()
+        {
+
+            if (curPointIndex.Count == 0) return;
+            Transform root = _selection.transform;
+            Mesh _selectMesh = kSelect.MESH;
+            int killID = curPointIndex[0];
+            if (_selectMesh == null && killID != -1) return;
+
+            Undo.SetSnapshotTarget(_selectMesh, "MeshVertexRemove");
+
+            List<Vector3> nVertices = new List<Vector3>(_selectMesh.vertices);
+            nVertices.RemoveAt(killID);
+
+            int lastVert = _selectMesh.triangles[_selectMesh.triangles.Length - 1];
+            List<int> removeList = new List<int>(); //RemoveTriangle(killID, new List<int>(_selectMesh.triangles));
+
+            for (int i = 0, n = _selectMesh.triangles.Length; i < n; i += 3)
+            {
+                int i1 = _selectMesh.triangles[i];
+                int i2 = _selectMesh.triangles[i + 1];
+                int i3 = _selectMesh.triangles[i + 2];
+
+                if (killID == 0)
+                {
+                    if (killID != i1 && killID != i2 && killID != i3)
+                        removeList.AddRange(new List<int> { _selectMesh.triangles[i] - 1, _selectMesh.triangles[i + 1] - 1, _selectMesh.triangles[i + 2] - 1 });
+                }
+                else if (killID == lastVert)
+                {
+                    if (killID != i1 && killID != i2 && killID != i3)
+                        removeList.AddRange(new List<int> { _selectMesh.triangles[i], _selectMesh.triangles[i + 1], _selectMesh.triangles[i + 2] });
+                }
+                else
+                {
+                    if (killID != i && killID != i + 1 && killID != i + 2)
+                    {
+                        removeList.AddRange(new List<int> { 
+                            _selectMesh.triangles[i]-1, 
+                            _selectMesh.triangles[i + 1]-1, 
+                            _selectMesh.triangles[i + 2] -1
+                        });
+
+                        /*if (removeList[i] < 0) removeList[i] = 0;
+                        if (removeList[i + 1] < 0) removeList[i + 1] = 0;
+                        if (removeList[i + 2] < 0) removeList[i + 2] = 0;*/
+                    }
+                //    Debug.Log((_selectMesh.triangles[i] - 1) + " " + (_selectMesh.triangles[i + 1] - 1) + " " + (_selectMesh.triangles[i + 2] - 1));
+                }
+            }
+            for (int i = 0, n = removeList.Count; i < n; i ++)
+                if (removeList[i] < 0) removeList[i] = 0;
+
+            _selectMesh.Clear();
+            _selectMesh.vertices = nVertices.ToArray();
+            _selectMesh.triangles = removeList.ToArray();
+        }
+
+        private static List<int> RemoveTriangle(int tindex, List<int> triangles)
+        {
+            List<int> removeList = new List<int>();
+            for (int i = 0, n = triangles.Count; i < n; i += 3)
+            {
+                if (triangles[i] != tindex &&
+                    triangles[i + 1] != tindex &&
+                    triangles[i + 2] != tindex)
+                {
+                    removeList.Add(i);
+                }
+            }
+            return removeList;
+        }
+        private static bool HasNeighbours(int id, Mesh mesh)
+        {
+            int t1 = id;
+            int t2 = id + 1;
+            int t3 = id + 2;
+
+            for (int i = 0, n = mesh.triangles.Length; i < n; i += 3)
+            {
+                if (id != i)
+                {
+                    int v1 = i;
+                    int v2 = i + 1;
+                    int v3 = i + 2;
+                    if (v1 == t1 || v2 == t2 || v3 == t3)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static void VERT_remove_2()
+        {
+            if (curPointIndex.Count > 0)
+            {
+                // _selection = kSelect.OBJECT;
+                Transform root = _selection.transform;
+                Mesh _selectMesh = kSelect.MESH;
+                int killID = curPointIndex[0];
+
+                if (_selectMesh == null && killID != -1)
+                {
+                    return;
+                }
+                Undo.SetSnapshotTarget(_selectMesh, "MeshVertexRemove");
+
+                List<Vector3> removeListV = new List<Vector3>();
+                List<int> removeList = new List<int>();
+
+                List<Vector3> nVertices = new List<Vector3>();
+                List<Vector2> nUvs = new List<Vector2>();
+                List<int> nTrias = new List<int>();
+
+
+                for (int i = 0, n = _selectMesh.triangles.Length; i < n; i += 3)
+                {
+
+                    if (_selectMesh.triangles[i] == killID ||
+                        _selectMesh.triangles[i + 1] == killID ||
+                        _selectMesh.triangles[i + 2] == killID)
+                    {
+                        removeList.Add(i);
+                        // Debug.Log("kill tri " + i +" "  );
+
+                        /*  if (_selectMesh.triangles[i] == killID)
+                          {
+                           
+                              removeListV.Add(_selectMesh.vertices[_selectMesh.triangles[i]]);
+                              //if (_selectMesh.triangles[i + 1] == killID) 
+                              removeListV.Add(_selectMesh.vertices[_selectMesh.triangles[i + 1]]);
+                             // if (_selectMesh.triangles[i + 2] == killID) 
+                              removeListV.Add(_selectMesh.vertices[_selectMesh.triangles[i + 2]]);
+                          }*/
+                    }
+                }
+                int inn = 0;
+                foreach (int vi in removeList)
+                {
+                    Debug.Log(inn + "  [ " + _selectMesh.triangles[vi] + ", " + _selectMesh.triangles[vi + 1] + ", " + _selectMesh.triangles[vi + 2] + " ]");
+                    inn++;
+                }
+
+                inn = 0;
+                /*  for (int i = 0, n = _selectMesh.triangles.Length; i < n; i+=3)
+                  {
+                    
+                     // Debug.Log(" Check if  " + i + " is odd : " + _selectMesh.triangles[i * 3] + " ? " + removeList.Contains(_selectMesh.triangles[i * 3])); 
+                      if (!removeList.Contains(_selectMesh.triangles[i * 3])||
+                          !removeList.Contains(_selectMesh.triangles[i * 3 +1 ]) ||
+                          !removeList.Contains(_selectMesh.triangles[i * 3 +2]))
+                      {
+                          Debug.Log("Add old " + i);
+                          nVertices.Add(_selectMesh.vertices[i]);
+                      }
+                  }
+                  nVertices = new List<Vector3>(_selectMesh.vertices);
+                  foreach( Vector3 v in removeListV ){
+                      nVertices.Remove(v);
+                  }
+                  int inn = 0;
+                  foreach (Vector3 v in nVertices)
+                  {
+                      Debug.Log(inn + " " + v);
+                      inn++;
+                  }
+                  int ia = 0;
+
+                  for (int i = 1, n = nVertices.Count; i < n; i++)
+                  {
+                      Debug.DrawLine(nVertices[i], nVertices[i - 1]);
+                      Debug.Log("nVerts " + i + " " + nVertices[i - 1] + " - " + nVertices[i]);
+                  }*/
+
+                // curPointIndex.RemoveAt(0);
+                nVertices.Add(_selectMesh.vertices[1]);
+                nVertices.Add(_selectMesh.vertices[2]);
+                nVertices.Add(_selectMesh.vertices[3]);
+                nVertices.Add(_selectMesh.vertices[5]);
+                nVertices.Add(_selectMesh.vertices[6]);
+                nVertices.Add(_selectMesh.vertices[7]);
+
+
+                Vector2[] uvs = new Vector2[nVertices.Count];
+                inn = 0;
+                foreach (Vector3 v in nVertices)
+                {
+                    uvs[inn] = new Vector2(v.x, v.z);
+                    inn++;
+                }
+                _selectMesh.Clear();
+                _selectMesh.vertices = nVertices.ToArray();
+                _selectMesh.uv = uvs;
+                _selectMesh.triangles = new int[6] { 0, 1, 3, 2, 4, 5 };
+
+                _selectMesh.RecalculateNormals();
+                _selectMesh.RecalculateBounds();
+
+
+            }
+        }
+
+
+
+
+        private static void VERT_remove_old()
+        {
+            if (curPointIndex.Count > 0)
+            {
+                // _selection = kSelect.OBJECT;
+                Transform root = _selection.transform;
+                Mesh _selectMesh = kSelect.MESH;
+                if (_selectMesh == null)
+                {
+                    return;
+                }
+
+                Undo.SetSnapshotTarget(_selectMesh, "MeshVertexRemove");
+
+                //
+                int killID = curPointIndex[0];
+                List<int> nList = new List<int>();
+                List<int> oList = new List<int>();
+                // find the neighboure tris
+                for (int i = 0, n = _selectMesh.triangles.Length; i < n; i += 3)
+                {
+
+                    //             Debug.Log(i+ " --- " + _selectMesh.triangles[i] + " " + _selectMesh.triangles[i+1] + " " + _selectMesh.triangles[i+2]);
+                    if (_selectMesh.triangles[i] == killID ||
+                        _selectMesh.triangles[i + 1] == killID ||
+                        _selectMesh.triangles[i + 2] == killID)
+                    {
+                        Debug.Log("Add " + i + " / " + _selectMesh.triangles[i]);
+                        nList.Add(i);
+                    }
+                    else
+                    {
+                        oList.Add(i);
+                    }
+                }
+
+                List<Vector2> verts2D = new List<Vector2>();
+                foreach (int k in nList)
+                {
+                    Vector3[] pLine = new Vector3[]{  
+                        root.TransformPoint( _selectMesh.vertices[_selectMesh.triangles[k]] ),
+                        root.TransformPoint( _selectMesh.vertices[_selectMesh.triangles[k+1] ] ),
+                        root.TransformPoint( _selectMesh.vertices[_selectMesh.triangles[k+2] ]) };
+
+                    if (k != killID) verts2D.Add(new Vector2(_selectMesh.vertices[_selectMesh.triangles[k]].x, _selectMesh.vertices[_selectMesh.triangles[k]].z));
+                    if (k + 1 != killID) verts2D.Add(new Vector2(_selectMesh.vertices[_selectMesh.triangles[k + 1]].x, _selectMesh.vertices[_selectMesh.triangles[k + 1]].z));
+                    if (k + 2 != killID) verts2D.Add(new Vector2(_selectMesh.vertices[_selectMesh.triangles[k + 2]].x, _selectMesh.vertices[_selectMesh.triangles[k + 2]].z));
+
+                    // Debug.Log("Draw : " + pLine[0] + " " +pLine[1] + " " +pLine[2]);
+                    /*     Debug.DrawLine(pLine[0], pLine[1]);
+                         Debug.DrawLine(pLine[2], pLine[1]);
+                         Debug.DrawLine(pLine[0], pLine[2]);
+
+     */
+                    // Handles.DrawAAPolyLine(pLine);
+                }
+                foreach (int k in oList)
+                {
+                    Vector3[] pLine = new Vector3[]{  
+                        root.TransformPoint( _selectMesh.vertices[_selectMesh.triangles[k]] ),
+                        root.TransformPoint( _selectMesh.vertices[_selectMesh.triangles[k+1] ] ),
+                        root.TransformPoint( _selectMesh.vertices[_selectMesh.triangles[k+2] ]) };
+
+                    // Debug.Log("Draw : " + pLine[0] + " " +pLine[1] + " " +pLine[2]);
+                    /*     Debug.DrawLine(pLine[0], pLine[1], Color.yellow);
+                         Debug.DrawLine(pLine[2], pLine[1], Color.yellow);
+                         Debug.DrawLine(pLine[0], pLine[2], Color.yellow);*/
+
+                    if (!verts2D.Contains(new Vector2(_selectMesh.vertices[_selectMesh.triangles[k]].x, _selectMesh.vertices[_selectMesh.triangles[k]].z)))
+                    {
+                        verts2D.Add(new Vector2(_selectMesh.vertices[_selectMesh.triangles[k]].x, _selectMesh.vertices[_selectMesh.triangles[k]].z));
+                    }
+                    if (!verts2D.Contains(new Vector2(_selectMesh.vertices[_selectMesh.triangles[k + 1]].x, _selectMesh.vertices[_selectMesh.triangles[k + 1]].z)))
+                    {
+                        verts2D.Add(new Vector2(_selectMesh.vertices[_selectMesh.triangles[k + 1]].x, _selectMesh.vertices[_selectMesh.triangles[k + 1]].z));
+                    }
+                    if (!verts2D.Contains(new Vector2(_selectMesh.vertices[_selectMesh.triangles[k + 2]].x, _selectMesh.vertices[_selectMesh.triangles[k + 2]].z)))
+                    {
+                        verts2D.Add(new Vector2(_selectMesh.vertices[_selectMesh.triangles[k + 2]].x, _selectMesh.vertices[_selectMesh.triangles[k + 2]].z));
+                    }
+                }
+
+
+
+                // create 2D vert list to triangulate
+                Triangulator tr = new Triangulator(verts2D.ToArray());
+                int[] indices = tr.Triangulate();
+                Vector3[] vertices = new Vector3[verts2D.Count];
+                for (int i = 0; i < vertices.Length; i++)
+                {
+
+                    vertices[i] = new Vector3(verts2D[i].x, 0, verts2D[i].y);
+                    if (i > 0) Debug.DrawLine(vertices[i], vertices[i - 1], Color.yellow);
+                }
+
+                /*
+
+                //      _selectMesh.Clear();
+                      _selectMesh.vertices = vertices;
+                      _selectMesh.triangles = indices;
+                      _selectMesh.RecalculateNormals();
+                      _selectMesh.RecalculateBounds();
+
+                      _selection.GetComponent<MeshCollider>().sharedMesh = null;
+                      _selection.GetComponent<MeshCollider>().sharedMesh = _selectMesh;
+                   */
+
+
+                /*       List<Vector3> lVerts = new List<Vector3>(_selectMesh.vertices);
+                       // delete verts
+                       foreach (int id in curPointIndex) lVerts.RemoveAt(id);
+                       // rebuild triangles and uvs
+                       List<int> lTris = new List<int>(lVerts.Count * 3);
+                       int index = 0;
+       */
+
+                /*          _selectMesh.Clear();
+                          _selectMesh.vertices = verts = vertices;
+                          _selectMesh.triangles = indices;
+                
+                           _selectMesh.RecalculateNormals();
+                           _selectMesh.RecalculateBounds();
+
+                           _selection.GetComponent<MeshCollider>().sharedMesh = null;
+                           _selection.GetComponent<MeshCollider>().sharedMesh = _selectMesh;*/
+
+            }
+        }
+
+
+
+    }//kP_edit end
+
+
     public class PLANAR_HELPER
     {
         public bool
@@ -461,7 +801,7 @@ namespace klock.kEditPoly.panels
             y_Axis = false,
             z_Axis = false;
     }
-}
+}//namespace end
 /*  switch (Tools.current)
   {
       case Tool.Move:
