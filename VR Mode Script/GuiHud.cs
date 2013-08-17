c5593e543d57847d2ea6905120e3ecbdb67329a2
kerd man <kerdman@online.de>
2013-08-17 16:26:03 +0200

    Change GUI position setting

--- VR Mode Script/GuiHud.cs
+++ VR Mode Script/GuiHud.cs
@@ -7,12 +7,17 @@ public class GuiHud : MonoBehaviour {
 	void Awake () {
 		Transform vr = transform.Find("vr_Logo");
 		GUITexture gt = vr.GetComponent<GUITexture>();
-		
-		gt.pixelInset = new Rect((Screen.width-290)*.5f,-(Screen.height-50)*.5f,128,53);
+
+        gt.pixelInset = new Rect(880, 45, 0, 0);
+        //gt.pixelInset = new Rect(0,0, -400, -250);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		Transform vr = transform.Find("vr_Logo");
+		GUITexture gt = vr.GetComponent<GUITexture>();
+
+       gt.pixelInset = new Rect(880, 45, 0, 0);
 	}
 }
